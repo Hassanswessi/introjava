8b782e3fe38e92f2d40b1ab1837c686bce1a48ed
jean dupont <jean.dupont@gmail.com>
2025-09-25 17:00:05 +0200

    M2E4

---
+++ src/M2E2.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class M2E2 {
+    public static void main(String[] args) {
+        String couleur = demanderLettreEtObtenirCouleur();
+        if (couleur.equals("connais pas")) {
+            System.out.println("Connais pas.");
+        } else {
+            System.out.println("Vous avez choisi le " + couleur + ".");
+        }
+    }
+
+    public static String demanderLettreEtObtenirCouleur() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quelle couleur souhaitez-vous ? ");
+        String lettre = scanner.nextLine();
+        String couleur;
+        if (lettre.equalsIgnoreCase("R")) {
+            couleur = "rouge";
+        } else if (lettre.equalsIgnoreCase("V")) {
+            couleur = "vert";
+        } else if (lettre.equalsIgnoreCase("B")) {
+            couleur = "bleu";
+        } else {
+            couleur = "connais pas";
+        }
+        return couleur;
+    }
+}
